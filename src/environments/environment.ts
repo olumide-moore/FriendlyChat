9b4cb7d8a967193511e2d85ea8e27ecbc1b59ea5
Mide Moore <olumide.moore2817@gmail.com>
2024-12-06 17:34:38 +0000

    Adding Google Authentication

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,12 +1,12 @@
 export const environment = {
   firebase: {
-    apiKey: 'API_KEY',
-    authDomain: 'PROJECT_ID.firebaseapp.com',
-    databaseURL: 'https://PROJECT_ID.firebaseio.com',
-    projectId: 'PROJECT_ID',
-    storageBucket: 'PROJECT_ID.appspot.com',
-    messagingSenderId: 'SENDER_ID',
-    appId: 'APP_ID',
-    measurementId: 'G-MEASUREMENT_ID',
+    apiKey: "AIzaSyBQcKqV6PaZUSvrp3fdY-dHF3omBgjaRko",
+    authDomain: "friendlychat-7f01f.firebaseapp.com",
+    projectId: "friendlychat-7f01f",
+    storageBucket: "friendlychat-7f01f.firebasestorage.app",
+    messagingSenderId: "1069351875873",
+    appId: "1:1069351875873:web:840bef53cba718e2ccfa60"
+    // databaseURL: 'https://PROJECT_ID.firebaseio.com',
+    // measurementId: 'G-MEASUREMENT_ID',
   },
 };
